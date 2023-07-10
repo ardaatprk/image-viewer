f85d0fe02165769341f37df9915dbbce2b5b3f6a
ardaatprk <ardaatprk@gmail.com>
2023-07-10 15:02:36 +0300

    Görseli ekrana yazdıran sistem görseli dsoya yolundan alıyor

--- image.py
+++ image.py
@@ -1,13 +1,15 @@
 import argparse
 import cv2
+
 def main():
+    parser =argparse.ArgumentParser()
+    parser.add_argument("TownCenter.jpg")
     ap = argparse.ArgumentParser()
-    ap.add_argument('image', help="patho to input image")
-    args = vars(ap.parse_args())
-    image=cv2.imread(args["image"])
-    cv2.imshow("TownCenter",image)
+    ap.add_argument('image', help="path to input image")
+    image=cv2.imread('/home/arda/intern/introduction/image-viewer/image/TownCenter.jpg')
+    cv2.imshow("image",image)
     cv2.waitKey(0)
+
 if __name__== '__main__':
     main()
     exit(0)
-
