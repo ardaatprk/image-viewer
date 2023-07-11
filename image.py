cfe9c68e3c6a8d54a6189afa55f3b4f1ca2a4d8f
ardaatprk <ardaatprk@gmail.com>
2023-07-11 10:19:04 +0300

    Dosya görselleri ekrana yazdıran kodlardaki hatalar giderildi

--- image.py
+++ image.py
@@ -2,14 +2,13 @@ import argparse
 import cv2
 
 def main():
-    parser =argparse.ArgumentParser()
-    parser.add_argument("TownCenter.jpg")
     ap = argparse.ArgumentParser()
-    ap.add_argument('image', help="path to input image")
-    image=cv2.imread('/home/arda/intern/introduction/image-viewer/image/TownCenter.jpg')
+    ap.add_argument("--image",required=True, help="path to input be displayed")
+    args = vars(ap.parse_args())
+    
+    image=cv2.imread(args["image"]) 
     cv2.imshow("image",image)
     cv2.waitKey(0)
 
 if __name__== '__main__':
-    main()
-    exit(0)
+    main() x
